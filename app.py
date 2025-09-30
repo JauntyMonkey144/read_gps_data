4e15c07db91308b95d8ed5ca3440d62e61a69ef5
Vinbobo <128805837+Vinbobo@users.noreply.github.com>
2025-09-30 13:43:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -216,7 +216,7 @@ def export_to_excel():
                 ws.cell(row=row, column=3 + j, value=entry)
 
             # Border + align cả dòng
-            for col in range(1, 13):
+            for col in range(1, 14):
                 cell = ws.cell(row=row, column=col)
                 cell.border = border
                 cell.alignment = align_left
